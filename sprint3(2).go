171205948b921a6af9f1a5297571ebccd089ba53
Stanislav <stas.kuchma.86@gmail.com>
2024-10-16 17:49:14 +0300

    test

--- sprint3(2).go
+++ sprint3(2).go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // )
 // //todo:типизация
 //
@@ -139,23 +141,23 @@ package main
 //	}
 
 // функция
-//func codew(code map[string][]int) {
-//	for key, value := range code {
-//		counter := 0.0
-//		for _, element := range value {
-//			counter += float64(element)
-//		}
-//		fmt.Println(key, counter/float64(len(value)))
-//	}
-//}
-//
-//// мэйн
-//func main() {
-//	mapp := map[string][]int{
-//		"stas":  {2, 5, 5, 5, 4},
-//		"vanya": {3, 4, 4, 3, 5},
-//		"danya": {4, 4, 4, 5, 5},
-//		"sanya": {2, 5, 5, 5, 4},
-//	}
-//	codew(mapp)
-//}
+func codew(code map[string][]int) {
+	for key, value := range code {
+		counter := 0.0
+		for _, element := range value {
+			counter += float64(element)
+		}
+		fmt.Println(key, counter/float64(len(value)))
+	}
+}
+
+// мэйн
+func main() {
+	mapp := map[string][]int{
+		"stas":  {2, 5, 5, 5, 4},
+		"vanya": {3, 4, 4, 3, 5},
+		"danya": {4, 4, 4, 5, 5},
+		"sanya": {2, 5, 5, 5, 4},
+	}
+	codew(mapp)
+}
